25373774c1800f4a29ee98d146ad6424201e1956
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-15 17:00:23 +0000

    game complete. refactoring stil to do.

--- hangman-game/hangman.go
+++ hangman-game/hangman.go
@@ -15,50 +15,106 @@ func main() {
 	var winner bool
 	var loser bool
 	var curGuess string
-	wrongGuesses := 0
+	var wrongGuesses int
+
 	reader := bufio.NewReader(os.Stdin)
 
 	refreshScreen(99)
-	fmt.Print("Setter, enter the secret word: ")
+	fmt.Print("PLAYER 1\nEnter the secret word: ")
 	word, _ = reader.ReadString('\n')
 	word = strings.TrimSuffix(word, "\r\n")
+	word = strings.ToLower(word)
 
-	for i := 0; i < len(word); i++ {
-		clue += "_ "
+	for range word {
+		clue += "_"
 	}
 
 	refreshScreen(wrongGuesses)
 	for !winner && !loser && curGuess != "exit" {
-		fmt.Printf("Here are your guesses so far: %q \n \n %s \n \n", prevGuesses, clue)
-
-		fmt.Print("Guesser, what is your first guess? ")
-		curGuess, _ = reader.ReadString('\n')
-		curGuess = strings.TrimSuffix(curGuess, "\r\n")
+		player2Question(prevGuesses, clue, curGuess, "", wrongGuesses)
+		curGuess = readCurGuess(curGuess, reader)
 
 		if curGuess != "exit" {
-
 			for !checkGuessLength(curGuess) || !checkGuessPrevious(curGuess, prevGuesses) {
-				if !checkGuessLength(curGuess) {
-					refreshScreen(wrongGuesses)
-					fmt.Printf("Here are your guesses so far: %q \n \n %s \n \n", prevGuesses, clue)
-					fmt.Printf("You enetered %q: you can only guess one letter! Try again: ", curGuess)
+				switch {
+				case !checkGuessLength(curGuess):
+					player2Question(prevGuesses, clue, curGuess, "You've entered too many letters! Try again: ", wrongGuesses)
+				case !checkGuessPrevious(curGuess, prevGuesses):
+					player2Question(prevGuesses, clue, curGuess, "You've already guessed this letter! Try again: ", wrongGuesses)
+				default:
 				}
-				if !checkGuessPrevious(curGuess, prevGuesses) {
-					refreshScreen(wrongGuesses)
-					fmt.Printf("Here are your guesses so far: %q \n \n %s \n \n", prevGuesses, clue)
-					fmt.Printf("You enetered %q: you've already guessed %q! Try again: ", curGuess, curGuess)
-				}
-				curGuess, _ = reader.ReadString('\n')
-				curGuess = strings.TrimSuffix(curGuess, "\r\n")
+				curGuess = readCurGuess(curGuess, reader)
 			}
 			prevGuesses = append(prevGuesses, curGuess)
-			wrongGuesses++
+			if letterCheck(curGuess, word) {
+				clue = updateClue(curGuess, clue, word)
+				if clue == word {
+					winner = true
+				}
+			} else {
+				wrongGuesses++
+			}
 		}
 		refreshScreen(wrongGuesses)
-		if wrongGuesses == 9 {
-			loser = true
+		if winner {
+			fmt.Printf("PLAYER 2\nHere are your guesses so far: %q \n \n %s \n \n", prevGuesses, buildPrintClue(clue))
+			fmt.Printf("Number of incorrect guesses so far: %d\n", wrongGuesses)
+			fmt.Print(`
+*******************
+*WE HAVE A WINNER!*
+*******************`)
+		} else {
+			if wrongGuesses == 9 {
+				loser = true
+			}
 		}
 	}
+}
+
+func player2Question(prevGuesses []string, clue, curGuess, errMessage string, wrongGuesses int) {
+	refreshScreen(wrongGuesses)
+	fmt.Printf("PLAYER 2\nHere are your guesses so far: %q \n \n %s \n \n", prevGuesses, buildPrintClue(clue))
+	fmt.Printf("Number of incorrect guesses so far: %d\n", wrongGuesses)
+	fmt.Printf(errMessage)
+}
+
+func readCurGuess(curGuess string, reader *bufio.Reader) string {
+	curGuess, _ = reader.ReadString('\n')
+	curGuess = strings.TrimSuffix(curGuess, "\r\n")
+	curGuess = strings.ToLower(curGuess)
+	return curGuess
+}
+
+func buildPrintClue(clue string) string {
+	var printClue string
+	for i := range clue {
+		printClue += string(clue[i])
+		printClue += " "
+	}
+	return printClue
+}
+
+func letterCheck(letter, word string) bool {
+	for i := range word {
+		if string(word[i]) == letter {
+			return true
+		}
+	}
+	return false
+}
+
+func updateClue(letter, clue, word string) string {
+	var newClue string
+	for i := range clue {
+		if string(word[i]) == letter {
+			newClue += letter
+		} else if string(clue[i]) == "_" {
+			newClue += "_"
+		} else {
+			newClue += string(word[i])
+		}
+	}
+	return newClue
 }
 
 func checkGuessLength(s string) bool {
